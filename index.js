c4a86c1e80729f89d9c8b47f198eb56d707e87a2
Long Nguyen <90327629+longnguyen1305@users.noreply.github.com>
2025-02-08 16:45:29 -0400

    Upload file API

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.use("/auth", require("./routes/jwtAuth"));
 
 app.use("/dashboard", require("./routes/dashboard"));
 
+// Project
+
+app.use("/project", require("./routes/project"))
+
 
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
---
+++ routes/project.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const authorization = require("../middleware/authorization");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+})
+const upload = multer({ storage });
+
+router.post("/upload", authorization, upload.single('zipfile'), async (req, res) => {
+    try {
+        
+        res.json(req.file);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+})
+
+module.exports = router;
