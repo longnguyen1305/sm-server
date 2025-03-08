ec1e8e188145694f5ff1cf23a5c7f17b3a4cf079
Long Nguyen <90327629+longnguyen1305@users.noreply.github.com>
2025-03-08 15:27:13 -0400

    Init basic retrieve metric images

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const pool = require("../db");
 const authorization = require("../middleware/authorization");
+const express = require("express");
+const fs = require("fs");
 
 router.get("/", authorization, async (req, res) => {
     try {
@@ -18,8 +20,32 @@ router.get("/", authorization, async (req, res) => {
 router.get("/project", authorization, async (req, res) => {
     try {
 
-        res.download('./results/RadixSpline.zip');
+        res.download('./results/projects/RadixSpline.zip');
         
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+})
+
+router.use('/metrics/images', express.static('./results/metrics/images'));
+
+router.get("/metrics", authorization, async (req, res) => {
+    try {
+
+        fs.readdir('./results/metrics/images', (err, files) => {
+            if (err) {
+                console.error("Error reading metrics folder:", err.message);
+                return res.status(500).json({ message: "Error fetching metrics" });
+            }
+
+            const images = files.map(file => ({
+                name: file,
+                url: `/metrics/images/${file}`
+            }));
+
+            res.json({ images });
+        })
     } catch (err) {
         console.error(err.message);
         res.status(500).send("Server Error");
