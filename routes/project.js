a16e7213b6436e1877c3312bbc34cec86bf28030
Long Nguyen <90327629+longnguyen1305@users.noreply.github.com>
2025-03-13 10:56:55 -0300

    Store input projects to Minio

--- routes/project.js
+++ routes/project.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const authorization = require("../middleware/authorization");
 const multer = require("multer");
+const pool = require("../db");
+const minioClient = require("../minioClient");
+
+require("dotenv").config();
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -14,8 +18,30 @@ const upload = multer({ storage });
 
 router.post("/upload", authorization, upload.single('zipfile'), async (req, res) => {
     try {
+      const file = req.file;
+
+      // Add new project to database
+      const projectQuery = `
+        INSERT INTO projects (user_id, project_name, project_status)
+        VALUES ($1, $2, 'pending') RETURNING *;
+      `;
+      const newProject = await pool.query(projectQuery, [req.user.id, file.originalname]);
+      const projectId = newProject.rows[0].project_id;
+
+      // Store file into MinIO
+      const bucketName = process.env.MINIO_BUCKET_NAME;
+      const filePath = `${req.user.id}/${projectId}/${file.originalname}`;
       
-      res.json(req.file);
+      await minioClient.fPutObject(bucketName, filePath, file.path);
+
+      // Add new folder to database
+      const folderQuery = `
+        INSERT INTO folders (project_id, folder_path_input)
+        VALUES ($1, $2) RETURNING *;
+      `;
+      const newFolder = await pool.query(folderQuery, [projectId, filePath]);
+
+      res.json({ success: true, message: 'File uploaded!', time: newFolder.rows[0].folder_createtime });
 
     } catch (err) {
       console.error(err.message);
