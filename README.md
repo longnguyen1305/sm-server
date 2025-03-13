812e4f8a91406f2e6f54af4e9f263158adc495cf
Long Nguyen <90327629+longnguyen1305@users.noreply.github.com>
2025-03-13 12:31:20 -0300

    API for get all projects and download one project

--- README.md
+++ README.md
@@ -3,15 +3,18 @@
 ## Setup Instructions
 
 - [Download Node.js](https://nodejs.org/en/download) - for Windows, MacOS, and Linux.
+- [Download MinIO](https://min.io/docs/minio/windows/index.html) - for Windows, MacOS, and Linux.
 
 ### 1. Clone the Repository
+
 - Run the following command to clone the project to your local system:
 
 ```sh
 git clone https://github.com/longnguyen1305/sm-server
 ```
 
-### 2. Install Dependencies  
+### 2. Install Dependencies
+
 - Navigate to the project directory and install the necessary dependencies:
 
 ```sh
@@ -19,15 +22,26 @@ cd sm-server
 npm install
 ```
 
-### 3. Getting Started  
+### 3. Getting Started
+
 - Once the dependencies are installed, you are ready to start working on the project.
 
 Run the server:
+
 ```sh
 node index
 ```
+
 or
+
 ```sh
 nodemon index
 ```
+
 - Nodemon can automatically restart server whenever there are changes saved.
+
+Run MinIO:
+
+```sh
+.\minio.exe server C:\<YOUR DIRECTORY>\minio\data --console-address ":9001"
+```
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,6 +3,9 @@ const pool = require("../db");
 const authorization = require("../middleware/authorization");
 const express = require("express");
 const fs = require("fs");
+const minioClient = require("../minioClient");
+
+require("dotenv").config();
 
 router.get("/", authorization, async (req, res) => {
     try {
@@ -17,11 +20,50 @@ router.get("/", authorization, async (req, res) => {
     }
 })
 
-router.get("/project", authorization, async (req, res) => {
+router.get("/projects", authorization, async (req, res) => {
     try {
-
-        res.download('./results/projects/RadixSpline.zip');
+        const userId = req.user.id;
         
+        const projectsQuery = `
+            SELECT project_id, project_name, project_status, project_createtime 
+            FROM projects 
+            WHERE user_id = $1 
+            ORDER BY project_createtime DESC;
+        `;
+        const projects = await pool.query(projectsQuery, [userId]);
+
+        if (projects.rows.length === 0) {
+            return res.json({ success: true, projects: [] });
+        }
+
+        res.json({ success: true, projects: projects.rows });
+        
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+})
+
+router.get("/projects/:projectId", authorization, async (req, res) => {
+    try {
+        const { projectId } = req.params;
+        const bucketName = process.env.MINIO_BUCKET_NAME;
+
+        const foldersQuery = `
+            SELECT folder_path_input FROM folders 
+            WHERE project_id = $1 
+            ORDER BY folder_createtime DESC;
+        `;
+        const result = await pool.query(foldersQuery, [projectId]);
+        const filePath = result.rows[0].folder_path_input;
+
+        minioClient.getObject(bucketName, filePath, (err, stream) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+            stream.pipe(res);
+        });
+
     } catch (err) {
         console.error(err.message);
         res.status(500).send("Server Error");
